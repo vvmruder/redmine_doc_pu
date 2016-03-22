461b7f90b03f235c5582e0b9250aadce094cb143
tommirintala <tommi.rintala@delektre.fi>
2016-03-22 12:27:18 +0200

    Missing '}' added
    Produced broken labels for chapters (missing }) in LaTeX code.

--- lib/latex_wiki_page.rb
+++ lib/latex_wiki_page.rb
@@ -53,7 +53,7 @@ module ModuleLatexWikiPage
 
 		if self.latex_add_chapter
 			# Add chapter tag
-			page_txt = "\n\\chapter{#{self.chapter_name}} \\label{page:#{self.wiki_page.title.gsub(' ', '_')}\n" + page_txt
+			page_txt = "\n\\chapter{#{self.chapter_name}} \\label{page:#{self.wiki_page.title.gsub(' ', '_')}}\n" + page_txt
 		else
 			# Add label to first section, if section exists
 			page_txt.sub!(/\\section\{(.+)\}/i) do |_|
